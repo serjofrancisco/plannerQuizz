c6bbc3a73c13e2721d4e7412fbf98f8809ed6fa5
Sergio Francisco <spinheiro.lf@gmail.com>
2024-11-13 16:41:14 -0300

    Monkey flip

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,29 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+// import styles from "./page.module.css";
+import fluxo from "../../src/fluxo.json"
+import { useState } from "react";
+import Perguntas from "../pages/Perguntas";
+import Respostas from "../pages/Respostas";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [resposta, setResposta] = useState("");
+  const [Dfluxo, setDfluxo] = useState(fluxo);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const handleResposta = ({target}) => {
+    setDfluxo(Dfluxo[Object.keys(Dfluxo)[0]][target.value]);
+    console.log(Object.values(Dfluxo))
+  }
+
+  return typeof Dfluxo !== "string" ? (
+    <div>
+    <h1>{Object.keys(Dfluxo)}</h1>
+      {Object.keys(Object.values(Dfluxo)[0]).map((item) => {
+        return <button key={item} value={item} onClick={handleResposta}>{item}</button>
+      }
+      )}
     </div>
-  );
+  ) : (<h1>{Dfluxo}</h1>)
+  
+  
+  ;
 }
