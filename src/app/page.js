db9ca036ce101da33ded6de99f5e9a1d2ba3ec8a
Sergio Francisco <spinheiro.lf@gmail.com>
2024-11-13 16:43:20 -0300

    Monkey flip

--- src/app/page.js
+++ src/app/page.js
@@ -2,16 +2,12 @@
 // import styles from "./page.module.css";
 import fluxo from "../../src/fluxo.json"
 import { useState } from "react";
-import Perguntas from "../pages/Perguntas";
-import Respostas from "../pages/Respostas";
 
 export default function Home() {
-  const [resposta, setResposta] = useState("");
   const [Dfluxo, setDfluxo] = useState(fluxo);
 
   const handleResposta = ({target}) => {
     setDfluxo(Dfluxo[Object.keys(Dfluxo)[0]][target.value]);
-    console.log(Object.values(Dfluxo))
   }
 
   return typeof Dfluxo !== "string" ? (
@@ -23,7 +19,5 @@ export default function Home() {
       )}
     </div>
   ) : (<h1>{Dfluxo}</h1>)
-  
-  
   ;
 }
